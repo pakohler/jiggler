8dc7e7bdfb99f42eed34130ed2e9afd44307051d
Paul Kohler <paul.kohler@shopify.com>
2023-12-05 12:08:45 -0800

    use flags and add go.mod

--- jiggler.go
+++ jiggler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"math/rand"
 	"math"
 	"time"
@@ -9,16 +10,20 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-const jigglemult = 5
-const delay = 15 * time.Second
-
 func main() {
+	var jigglemult = flag.Int("pixels", 5, "maximum amount of x or y jiggle")
+	var delay = flag.Duration("delay", 15*time.Second, "time between jiggles")
+	var click = flag.Bool("click", false, "whether to click or not")
+	flag.Parse()
 	log := log.New(os.Stdout, "jiggler: ", log.Ldate | log.Ltime | log.Lshortfile)
 	for {
-		dx := int(math.Round(rand.NormFloat64() * jigglemult))
-		dy := int(math.Round(rand.NormFloat64() * jigglemult))
+		dx := int(math.Round(rand.NormFloat64() * float64(*jigglemult)))
+		dy := int(math.Round(rand.NormFloat64() * float64(*jigglemult)))
 		log.Printf("moving mouse %dpx horizontally and %dpx vertically", dx, dy)
 		robotgo.MoveSmoothRelative(dx, dy)
-		time.Sleep(1 * delay)
+		if *click {
+			robotgo.Click()
+		}
+		time.Sleep(1 * *delay)
 	}
 }
